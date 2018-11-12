550e2fa8e36eb873f826723ded6899e9f8e30ac4
Gianni De Fabritiis <g.defabritiis@gmail.com>
2018-11-12 17:41:54 +0100

    Update nin.py

--- models/nin.py
+++ models/nin.py
@@ -7,7 +7,7 @@ class NiN(nn.Module):
     def __init__(self):
         super(NiN, self).__init__()
         self.classifer = nn.Sequential(
-                nn.Conv2d(3, 192, kernel_size=5, stride=1, padding=2),
+                nn.Conv2d(3, 192, kernel_size=3, stride=1, padding=1),
                 nn.ReLU(inplace=True),
                 nn.Conv2d(192, 160, kernel_size=1, stride=1, padding=0),
                 nn.ReLU(inplace=True),
@@ -16,7 +16,7 @@ class NiN(nn.Module):
                 nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
                 nn.Dropout(0.5),
 
-                nn.Conv2d(96, 192, kernel_size=5, stride=1, padding=2),
+                nn.Conv2d(96, 192, kernel_size=3, stride=1, padding=1),
                 nn.ReLU(inplace=True),
                 nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
                 nn.ReLU(inplace=True),
